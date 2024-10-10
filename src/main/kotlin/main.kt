267bb71e63da7b317d17250dd4da7a0cd8cf5e4c
m1narty <jsjake4770@gmail.com>
2024-10-10 13:45:44 +0000

    add New codes

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,4 +1,11 @@
 //import java.util.Date
 fun main(){
+    val size = readLine()!!.toInt()
+    val square = Array(size) { (it + 1)*(it + 1) } // 람다식 활용
+    for (value in square) {
+        println(value)
+    }
 
+    val operations = charArrayOf('+', '-', '*', '/', '%')
+    val squares = IntArray(10){ (it + 1)*(it + 1)}
 }
