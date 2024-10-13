723126520fccb05be653f5f8c2c5f48cfe81e769
m1narty <jsjake4770@gmail.com>
2024-10-13 13:26:56 +0000

    add New codes

--- src/main/kotlin/func.kt
+++ src/main/kotlin/func.kt
@@ -1,12 +1,34 @@
-import kotlin.math.PI
+//import kotlin.math.PI
+//
+//fun circleArea(radius: Double): Double {
+//    return PI*radius*radius
+//}
+//
+//fun main() {
+//    print("Enter radius: ")
+//    val radius = readLine()!!.toDouble()
+//    println("Circle area: ${circleArea(radius)}")
+//}
+// This function calculates the area of a circle given its radius. The main function takes user input for the radius and calls the circleArea function to calculate and print the area of the circle.
 
-fun circleArea(radius: Double): Double {
-    return PI*radius*radius
+//fun increment(a: IntArray): Int {
+//    return ++a[0]
+//}
+//
+//fun main() {
+//    val a = intArrayOf(1, 2, 3)
+//    println(increment(a))
+//    println(a.contentToString())
+//}
+
+fun rectangleArea(width: Double, height: Double): Double {
+    return width*height
 }
 
 fun main() {
-    print("Enter radius: ")
-    val radius = readLine()!!.toDouble()
-    println("Circle area: ${circleArea(radius)}")
+    val w = readLine()!!.toDouble()
+    val h = readLine()!!.toDouble()
+    println("Rectangle area: ${rectangleArea(w, h)}")
+    println("Rectangle area: ${rectangleArea(width = w, height = h)}")
+    println("Rectangle area: ${rectangleArea(height = h, width = w)}")
 }
-// This function calculates the area of a circle given its radius. The main function takes user input for the radius and calls the circleArea function to calculate and print the area of the circle.
