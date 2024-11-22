cebcf911d0c9309b7f7b2cd168fac4c8abce68f1
m1narty <jsjake4770@gmail.com>
2024-11-22 14:16:46 +0000

    learning Conditional Statement

--- src/main/kotlin/IntervalRange.kt
+++ src/main/kotlin/IntervalRange.kt
@@ -1,6 +1,6 @@
 fun main() {
     val chars = 'a' .. 'h'    // 'a'부터 'h'까지의 모든 문자
-//  val twoDigits = 10 .. 99  // 10 부터 99까지의 모든 수
+//    val twoDigits = 10 .. 99        // 10 부터 99까지의 모든 수
     val zero2One = 0.0 .. 1.0 // 0부터 1까지의 모든 부동소수점 수
 
 //    val num = readLine()!!.toInt()
