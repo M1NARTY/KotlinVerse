5fa4c7e18ba4cb2701b094f7d05427a77a33c822
m1narty <jsjake4770@gmail.com>
2024-12-09 14:09:38 +0000

    2024.12.09 오류 수정

--- src/main/kotlin/NumberDescription.kt
+++ src/main/kotlin/NumberDescription.kt
@@ -11,7 +11,7 @@ fun numberDescription2(n: Int, max: Int = 100): String = when (n) {
     0 -> "Zero"
     1, 2, 3 -> "Small"
     in 4..9 -> "Medium"
-    in 10..max _> "Large"
+    in 10..max -> "Large"
     !in Int.MIN_VALUE until 0 -> "Negative"
     else -> "Huge"
 }
